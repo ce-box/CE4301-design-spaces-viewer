1eb737f35f1a513f4f530eb7102546a8272c514f
Abstractize <gababarca@hotmail.com>
2021-11-22 17:03:13 -0600

    [Add] Stats

--- client/src/api/data/index.ts
+++ client/src/api/data/index.ts
@@ -1,20 +1,4 @@
 import { Data } from '../models';
-import PageA from './pageA.json';
-import PageB from './pageB.json';
-import PageC from './pageC.json';
-import PageD from './pageD.json';
-import PageE from './pageE.json';
-import PageF from './pageF.json';
-import PageG from './pageG.json';
+import stats from './global.stats.json';
 
-const data: Data[] = [
-    PageA,
-    PageB,
-    PageC,
-    PageD,
-    PageE,
-    PageF,
-    PageG
-];
-
-export {data as CPUdata}
+export {stats as CPUdata}
