011194c6096dc34824f7d749cb39318f5d6ffd66
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2021-11-24 13:23:59 -0600

    [add] New benchmark added, changed to sjeng. Also compiled for arm and riscv

---
+++ benchmarks/spec/458.sjeng/arm/runGem5.sh
@@ -0,0 +1,6 @@
+# -- an example to run SPEC 429.mcf on gem5, put it under 429.mcf folder --
+export GEM5_DIR=/home/ealvarado/Documents/tec/ce4301-arquitectura-computadores/GEM5/gem5
+export BENCHMARK=./src/benchmark
+export ARGUMENT=./data/test.txt
+
+time $GEM5_DIR/build/ARM/gem5.opt -d m5out/ $GEM5_DIR/configs/example/se.py -c $BENCHMARK -o $ARGUMENT -I 100000000 --cpu-type=TimingSimpleCPU --caches
