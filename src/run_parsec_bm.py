6841da16d91d8f302ac65a73bd6753d74e8825e4
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2021-11-25 07:31:56 -0600

    [add] Scripts for automation of tests, environment variables and modifications of the BPU

---
+++ src/run_parsec_bm.py
@@ -0,0 +1,97 @@
+import os
+from shutil import copyfile
+
+GEM5_DIR = os.getenv('GEM5_DIR')
+BENCHMARK_DIR = os.getenv('PARSEC_BENCHMARK_DIR')
+
+config = {
+    'output_dir':'m5out',
+    'isa':'arm',
+    'cpu':'TimingSimpleCPU'
+}
+
+
+def run_parsec_bm(cpu='TimingSimpleCPU', isa='ARM', max_instructions=100000000):
+
+    '''
+    
+    '''
+
+    config['isa'] = isa.lower()
+    config['cpu'] = cpu.lower()
+    
+    bm = BENCHMARK_DIR
+    _isa = config['isa']
+    out = config['output_dir']
+
+    benchmark = f'{bm}/{_isa}/src/blackscholes'
+    arg = f'1 {bm}/{_isa}/inputs/input_test/in_4.txt pruebas'
+    base_command = f'sudo time {GEM5_DIR}/build/{isa}/gem5.opt -d {out} {GEM5_DIR}/configs/example/se.py -I {max_instructions} -c {benchmark} -o {arg} --cpu-type={cpu} --caches'
+
+    run_default(base_command)
+    run_cache_size_variations(base_command)
+    run_cache_assoc_variations(base_command)
+
+
+def run_default(base_cmd):
+
+    '''
+    '''
+    
+    os.system(base_cmd)
+    rename_stats_file('branch-predictor','tournament')
+
+
+def run_cache_size_variations(base_cmd):
+
+    '''
+    '''
+    
+    caches = ['1kB','2kB','4kB','8kB','16kB']
+    for cache_size in caches:
+        cmd = f'{base_cmd} --l2cache --l1d_size={cache_size} --l1i_size={cache_size}'
+        os.system(cmd)
+        rename_stats_file('cache-size',cache_size)
+
+
+def run_cache_assoc_variations(base_cmd):
+
+    '''
+    '''
+    
+    caches = ['1','2','4','8','16']
+    for cache_assoc in caches:
+        cmd = f'{base_cmd} --l2cache --l1d_assoc={cache_assoc} --l1i_assoc={cache_assoc}'
+        os.system(cmd)
+        rename_stats_file('cache-assoc',cache_assoc)
+
+
+def rename_stats_file(test, value):
+
+    '''
+    '''
+    
+    bm = BENCHMARK_DIR
+    isa = config['isa']
+    out = config['output_dir']
+    cpu = config['cpu']
+
+    # copy stats.txt
+    original_stats_file = f'./m5out/stats.txt'
+    new_stats_file = f'{bm}/{isa}/{out}/stats_{cpu}:{isa}:{test}:{value}.txt'
+    copyfile(original_stats_file, new_stats_file)
+
+    # copy config.ini
+    original_config_file = f'./m5out/config.ini'
+    new_config_file = f'{bm}/{isa}/{out}/config_{cpu}:{isa}:{test}:{value}.ini'
+    copyfile(original_config_file, new_config_file)
+    
+
+def run(isa):
+    cpu_types = ['TimingSimpleCPU','O3CPU']
+    for cpu in cpu_types:
+        run_parsec_bm(cpu, isa)
+
+
+if __name__ == '__main__':
+    run('RISCV')
---
+++ src/setup.sh
@@ -0,0 +1,3 @@
+export GEM5_DIR=/home/ealvarado/Documents/tec/ce4301-arquitectura-computadores/GEM5/gem5
+export PARSEC_BENCHMARK_DIR=/home/ealvarado/Documents/tec/ce4301-arquitectura-computadores/proyecto-03/benchmarks/parsec/blackscholes
+export SPEC_BENCHMARK_DIR=/home/ealvarado/Documents/tec/ce4301-arquitectura-computadores/proyecto-03/benchmarks/spec/458.sjeng
